bcb11dc0463a6299df5f7cd83f5b1a9582d8c5f6
value_ga <odr3045@gmail.com>
2025-02-06 20:35:04 +0900

    customer(models.py)

--- sparta_furniture/customer/models.py
+++ sparta_furniture/customer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# 고객
+class Customer(models.Model):
+    customer_id = models.CharField(max_length=50, primary_key= True) # 고객 id
+    customer_name = models.CharField(max_length=50)                  # 고객 이름
+    customer_create_at = models.DateField(auto_now_add=True)         # 고객 등록 날짜
+    customer_tel = models.CharField(max_length=50)                   # 고객 전화번호
